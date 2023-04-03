3b70a0b1a5d12584ac8a07157dc625383f381852
amandagpearce <amandagpearce@gmail.com>
2023-04-03 08:35:37 -0300

    next app + bootstrap

---
+++ fitter-happier/src/pages/_app.js
@@ -0,0 +1,6 @@
+import "@component/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
