1bf1617d513c994c526c6805836694129d498f91
amandagpearce <amandagpearce@gmail.com>
2023-04-21 15:31:21 -0300

    my goals section wip

--- src/components/main/MyGoals.js
+++ src/components/main/MyGoals.js
@@ -3,31 +3,59 @@ import Select from "../ui/Select";
 import Graph from "../ui/Graph";
 
 const MyGoals = () => {
+  var DUMMY_EXERCISES = [
+    {
+      type: "A",
+      goal: 5,
+      measuredBy: "month",
+      achieved: 4,
+    },
+    {
+      type: "B",
+      goal: 10,
+      measuredBy: "month",
+      achieved: 2,
+    },
+    {
+      type: "C",
+      goal: 15,
+      measuredBy: "month",
+      achieved: 7,
+    },
+    {
+      type: "C",
+      goal: 15,
+      measuredBy: "month",
+      achieved: 7,
+    },
+  ];
+
+  let totalGoals = 0;
+  let achievedGoals = 0;
+
+  DUMMY_EXERCISES.map((exercise) => {
+    totalGoals = totalGoals + exercise.goal;
+    achievedGoals = achievedGoals + exercise.achieved;
+  });
+
   return (
     <React.Fragment>
       <div className="col-12">
         <Select
           id="exercise-goals"
           label="Minhas metas de exercício"
-          options={["ÚLTIMO MÊS", "3 MESES", "6 MESES", "1 ANO"]}
+          options={["ÚLTIMO MÊS", "3 MESES"]}
         />
       </div>
       <div className="row">
         <div className="col-xs-12 col-md-4">
           <p>X% de metas atingidas</p>
-          <p>x/x Treinos completos</p>
+          <p>
+            {achievedGoals}/{totalGoals} Treinos completos
+          </p>
         </div>
         <div className="col-xs-9 col-md-6">
-          <div
-            className="graphs-wrapper"
-            style={{
-              width: "410px",
-              height: "410px",
-              position: "relative",
-            }}
-          >
-            <Graph />
-          </div>
+          <Graph exercises={DUMMY_EXERCISES} />
         </div>
         <div className="col-xs-3 col-md-2"></div>
       </div>
