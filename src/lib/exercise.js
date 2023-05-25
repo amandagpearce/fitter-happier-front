90f2fb1f46f368b0516e38373542728678a32733
Amanda Pearce <amandagpearce@gmail.com>
2023-05-25 18:50:54 -0300

    refactoring await calls

--- src/lib/exercise.js
+++ src/lib/exercise.js
@@ -66,8 +66,6 @@ export async function deleteVideo(data) {
     requestOptions
   );
 
-  console.log("deleted");
-
   return response;
 }
 
@@ -87,12 +85,32 @@ export async function deleteExercise(id) {
     requestOptions
   );
 
-  console.log("deleted");
-
   return response;
 }
 
 export async function addNewExercise(data) {
+  const myHeaders = new Headers(),
+    raw = JSON.stringify(data);
+
+  myHeaders.append("Content-Type", "application/json");
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  const response = await fetch(
+    "http://localhost:5000/exercise",
+    requestOptions
+  );
+  const result = await response.json();
+
+  return { newExerciseId: result.id };
+}
+
+export async function addNewVideo(data) {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   const raw = JSON.stringify(data);
@@ -104,56 +122,30 @@ export async function addNewExercise(data) {
     redirect: "follow",
   };
 
-  const response = await fetch(
-    "http://127.0.0.1:5000/exercise",
-    requestOptions
-  );
-
+  const response = await fetch("http://localhost:5000/video", requestOptions);
   const result = await response.json();
-
-  return { newExerciseId: result.id };
-}
-
-export async function addNewVideo(data) {
-  var myHeaders = new Headers();
-
-  myHeaders.append("Content-Type", "application/json");
-
-  var raw = JSON.stringify(data),
-    newId;
-
-  var requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
-
-  var response = await fetch("http://localhost:5000/video", requestOptions)
-    .then((response) => response.json())
-    .then((result) => (newId = result.id))
-    .catch((error) => console.log("error", error));
+  const newId = result.id;
 
   return { newVideoId: newId };
 }
 
 export async function addVideoToExercise(data) {
-  var myHeaders = new Headers();
+  const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
-  var requestOptions = {
+  const requestOptions = {
     method: "POST",
     headers: myHeaders,
     redirect: "follow",
   };
 
-  var response = await fetch(
+  const url =
     "http://localhost:5000/exercise/" +
-      data.exercise_id +
-      "/video/" +
-      data.video_id,
-    requestOptions
-  );
+    data.exercise_id +
+    "/video/" +
+    data.video_id;
+
+  const response = await fetch(url, requestOptions);
 
   return response;
 }
